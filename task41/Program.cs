fa8b06705b87d052438f817be80fb608b326dc0f
Pavel <you@example.com>
2023-07-25 12:19:12 +0300

    init commit

---
+++ task41/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int GetPositiveNumber(double[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    return count;
+}
+
+double[] InpootArray(int size)
+{
+    double[] mas = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        mas[i] = Convert.ToDouble(Console.ReadLine());
+    }
+    return mas;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = GetInput("Сколбко чисел желаете ввести: ");
+double[] array = InpootArray(size);
+Console.WriteLine($"{string.Join(", ", array)} -> {GetPositiveNumber(array)}");
+
---
+++ task43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindTheIntersectionPoint(double k1, double b1, double k2, double b2, out double x, out double y)
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+}
+
+double GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double b1 = GetInput("Введите b1: ");
+double k1 = GetInput("Введите k1: ");
+double b2 = GetInput("Введите b2: ");
+double k2 = GetInput("Введите k2: ");
+double x, y;
+FindTheIntersectionPoint(k1, b1, k2, b2, out x, out y);
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x} , {y}) ");
